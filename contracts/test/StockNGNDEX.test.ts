05596a0c9dc3baf98a995484b2907f15bff67988
walterthesmart <nwaugowalter@gmail.com>
2025-08-05 18:00:17 +0100

    test: enhance DEX testing capabilities
    - Add time manipulation for testing time-based functions
    - Improve test coverage for DEX operations
    - Add advanced testing utilities
    - Prepare for comprehensive DEX testing suite

--- contracts/test/StockNGNDEX.test.ts
+++ contracts/test/StockNGNDEX.test.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { StockNGNDEX, NGNStablecoin, NigerianStockToken } from "../typechain-types";
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
 
